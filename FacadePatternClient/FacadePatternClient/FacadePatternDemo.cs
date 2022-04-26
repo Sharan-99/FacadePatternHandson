74f0979ce767c797576eccecb09c205f8fd59798
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 10:55:32 +0530

    first commit

---
+++ FacadePatternClient/FacadePatternClient/FacadePatternDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacadePatternLibrary;
+
+namespace FacadePatternClient
+{
+    class FacadePatternDemo
+    {
+        static void Main(string[] args)
+        {
+            ShapeMaker shapeMaker = new ShapeMaker();
+
+            shapeMaker.DrawCircle();
+            shapeMaker.DrawRectangle();
+            shapeMaker.DrawSquare();
+            Console.ReadKey();
+        }
+    }
+}
